dd8f3e5efdedd3cdee280358bb444ea3fd7edae6
Nikola Josipović <nikola@cikloberza.com>
2020-10-30 10:08:39 +0100

    Update CacheProfile.php
    If no cache hit search in higher priority caches.. ie if not has no hit search in anon and auth user caches.

--- src/Hautelook/Solarium/Cache/CacheProfile.php
+++ src/Hautelook/Solarium/Cache/CacheProfile.php
@@ -9,8 +9,15 @@ class CacheProfile
 {
     private $key;
     private $lifetime;
+    private $keyPriorities = array();
 
-    public function __construct($key, $lifetime)
+    /**
+     * CacheProfile constructor.
+     * @param $key
+     * @param $lifetime
+     * @param string[]|null $keyPriorities Priorities with bot key being first entry and auth user key the last
+     */
+    public function __construct($key, $lifetime, $keyPriorities)
     {
         if (null === $lifetime) {
             throw new \InvalidArgumentException('You need to give a lifetime for the cache.');
@@ -18,6 +25,7 @@ class CacheProfile
 
         $this->key = $key;
         $this->lifetime = $lifetime;
+        $this->keyPriorities = empty($keyPriorities) ? array() : $keyPriorities;
     }
 
     public function getKey()
@@ -33,5 +41,13 @@ class CacheProfile
     public function getLifetime()
     {
         return $this->lifetime;
+    }
+
+    /**
+     * @return array|string[]
+     */
+    public function getKeyPriorities()
+    {
+        return $this->keyPriorities;
     }
 }
